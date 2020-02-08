811f116d4facc6fbaa85d4e0f935033d8f503274
kingsley okpara <kingsconsult001@gmail.com>
2020-02-08 03:20:53 +0100

    update

---
+++ decagoncentral/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello Kings
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
