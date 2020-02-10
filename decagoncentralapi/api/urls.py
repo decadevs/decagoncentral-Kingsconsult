976410aeb431579b2dbb274c85e86e00ddda858f
kingsley okpara <kingsconsult001@gmail.com>
2020-02-10 16:40:09 +0100

    created other routes and views

--- decagoncentralapi/api/urls.py
+++ decagoncentralapi/api/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ListUser.as_view()),
+    path('login/', views.UserList.as_view(), name="auth-login"),
+    path('<int:pk>/', views.UserDetail.as_view()), 
+    path('login/', views.LoginView.as_view(), name="auth-login"),
     path('register/', views.RegisterUsersView.as_view(), name="auth-register")
 ]
